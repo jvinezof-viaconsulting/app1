1ade16e01156f6fef8aa05808bcbfa94939d0c7f
João Vinezof <joaovitorvinezof@gmail.com>
2025-10-09 10:26:04 -0300

    feat: remove next multi-zone proxy

--- next.config.ts
+++ next.config.ts
@@ -1,21 +1,20 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  basePath: '',
-  async rewrites() {
-    return {
-      beforeFiles: [
-        {
-          source: '/app2',
-          destination: `${process.env.NEXT_PUBLIC_APP2_URL}/app2`,
-        },
-        {
-          source: '/app2/:path*',
-          destination: `${process.env.NEXT_PUBLIC_APP2_URL}/app2/:path*`,
-        },
-      ],
-    }
-  },
+  // async rewrites() {
+  //   return {
+  //     beforeFiles: [
+  //       {
+  //         source: '/app2',
+  //         destination: `${process.env.NEXT_PUBLIC_APP2_URL}/app2`,
+  //       },
+  //       {
+  //         source: '/app2/:path*',
+  //         destination: `${process.env.NEXT_PUBLIC_APP2_URL}/app2/:path*`,
+  //       },
+  //     ],
+  //   }
+  // },
 };
 
 export default nextConfig;
